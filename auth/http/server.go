f24436cba3f9c0ab31f135f2cc464df9d7cc3f9b
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2025-04-11 00:43:40 +0500

    chore: added envs for auth

--- auth/http/server.go
+++ auth/http/server.go
@@ -1,26 +1,32 @@
 package http
 
 import (
-	"github.com/IbraheemHaseeb7/fyp-backend/http/routers"
-	"github.com/IbraheemHaseeb7/fyp-backend/utils"
-	"github.com/IbraheemHaseeb7/fyp-backend/validation"
-	"github.com/IbraheemHaseeb7/pubsub"
-	"github.com/go-playground/validator/v10"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+  "os"
+
+  "github.com/IbraheemHaseeb7/fyp-backend/http/routers"
+  "github.com/IbraheemHaseeb7/fyp-backend/utils"
+  "github.com/IbraheemHaseeb7/fyp-backend/validation"
+  "github.com/IbraheemHaseeb7/pubsub"
+  "github.com/go-playground/validator/v10"
+  "github.com/labstack/echo/v4"
+  "github.com/labstack/echo/v4/middleware"
 )
 
 func StartHTTPServer(p *pubsub.Publisher) {
-	utils.Requests = make(map[string]chan pubsub.PubsubMessage)
+  utils.Requests = make(map[string]chan pubsub.PubsubMessage)
 
-	validation.Validate = validator.New(validator.WithRequiredStructEnabled())
-	validation.Validate.RegisterValidation("reg-no", validation.IsComsatsRegistrationNumber)
-	validation.Validate.RegisterValidation("password", validation.IsPassword)
+  validation.Validate = validator.New(validator.WithRequiredStructEnabled())
+  validation.Validate.RegisterValidation("reg-no", validation.IsComsatsRegistrationNumber)
+  validation.Validate.RegisterValidation("password", validation.IsPassword)
 
-	e := echo.New()
-	e.Use(middleware.CORS())
+  e := echo.New()
+  e.Use(middleware.CORS())
 
-	routers.ApiRouter(e, p)
+  routers.ApiRouter(e, p)
 
-	e.Logger.Fatal(e.Start("192.168.1.8:8000"))
+  if os.Getenv("ENVIRONMENT") == "local" {
+    e.Logger.Fatal(e.Start(os.Getenv("PORT")))
+  } else if os.Getenv("ENVIRONMENT") == "staging" {
+    e.Logger.Fatal(e.Start(os.Getenv("BASE_ADDRESS")+os.Getenv("PORT")))
+  }
 }
