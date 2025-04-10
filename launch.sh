4b4d3e40a03ee25ba8d88feb928388a9258932e5
IbraheemHaseeb7 <ibraheemhaseeb7@gmail.com>
2025-04-11 00:44:14 +0500

    chore: launch file for whole backend

---
+++ launch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# starting auth service
+echo "Starting auth service..."
+cd auth && air > /dev/null 2>&1 &
+
+# obtaining auth service process id
+pid=$!
+
+# display success message of starting auth service with process id
+echo "Successfully started auth service...Process ID: $pid"
+
+
+# starting db service
+echo "Starting db service..."
+cd ../db && air > /dev/null 2>&1 &
+
+# obtaining db service process id
+pid=$!
+
+# display success message of starting db service with process id
+echo "Successfully started db service...Process ID: $pid"
